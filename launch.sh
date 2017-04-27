5f0110b4ba56e1a5b8a53920b9e7a95f29c2919e
Siddharth Pathak <pathak.sid03@gmail.com>
2017-04-27 03:02:23 -0400

    added 2 clients by default

--- launch.sh
+++ launch.sh
@@ -8,5 +8,4 @@ echo "Server and Client Terminals will now open.."
 gnome-terminal -e "bash -c \"python server.py; exec bash\""
 
 gnome-terminal -e "bash -c \"telnet localhost 8888; exec bash\""
-
 gnome-terminal -e "bash -c \"telnet localhost 8888; exec bash\""
