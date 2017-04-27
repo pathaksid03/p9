d37a2d17ac2d1eba5a9de0a09904a73eb303b587
Siddharth Pathak <pathak.sid03@gmail.com>
2017-04-27 03:00:40 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -8,3 +8,5 @@ echo "Server and Client Terminals will now open.."
 gnome-terminal -e "bash -c \"python server.py; exec bash\""
 
 gnome-terminal -e "bash -c \"telnet localhost 8888; exec bash\""
+
+gnome-terminal -e "bash -c \"telnet localhost 8888; exec bash\""
